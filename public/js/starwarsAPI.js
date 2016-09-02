3b635a54b14db345cab8b3e9c993c89dfaf13ad9
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-09-02 12:32:16 -0500

    I need to fix the planets keeps repeating itself

--- public/js/starwarsAPI.js
+++ public/js/starwarsAPI.js
@@ -19,8 +19,10 @@ $(document).ready(function() {
 	var $content = $('#content');
 	var $links = $('.generalOptions');
 	var $table = $('#tableContent');
-	var $dropDownPeople = $(".1-100");
-	var $dropDownPlanets = $(".1-61");
+	//Dropdown lists
+	var $dropDownPeople = $(".dropDownPeople");
+	var $dropDownPlanets = $(".dropDownPlanets");
+	//*******************************
 	var $divContent = $('#divContent');
 	var $spanInfo = $('#spanInfo');
 	//arr that has all the dropdowns...keep adding as we develop
@@ -31,6 +33,19 @@ $(document).ready(function() {
 	var infoPlanets = "<tr><th>Name</th><th>Terrain</th> <th>Climate</th><th>Gravity</th><th>Population</th><th>Residents</th></tr>";
 	
 
+	//this function will return true if we are going from a bigger number to a smaller in our dropdown
+	function comingFromBigToSmallInDropDownList(newNumber){
+		var number = 1;
+		var temp = number;//saves the last number
+		number = newNumber//assigns it to the new number required to display
+		//if loop if the new number is smaller than the old one we reset what we have and start over again
+		if(temp < newNumber){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 	//draw the planets
 	function drawPlanets(planet){
 		infoPlanets += "<tr>"+
@@ -46,6 +61,7 @@ $(document).ready(function() {
 		$table.html(infoPlanets);
 	}
 	function ajaxForPlanets(){
+
 		/////////////Initial Values for every anchor??///////////////////
 		createDropDown(maxNumberOfPlanets, $dropDownPlanets);
 		//dealing with drop downs make function
@@ -56,8 +72,15 @@ $(document).ready(function() {
 		$divContent.show();
 		/////////////Initial Values for every anchor??///////////////////
 
+		//do this once which is showing the first element once
+		// do{
+		// 	swapiModule.getPlanet(2,function(planet) {
+		// 		drawPlanets(planet);
+		// 	});
+		// }while(3>4);
 		$dropDownPlanets.on( "change", function(){
 			var numberPlanets = $(this).val();
+
 			for(var i=1; i < numberPlanets; i++){//is just printing one planet in i the number of i FIX THIS
 				swapiModule.getPlanet(i,function(planet) {
 				    drawPlanets(planet);
@@ -73,7 +96,6 @@ $(document).ready(function() {
 			if(arrayDropDowns[i] == $notMe){//the one we exclude'
 				//also show it.
 				$notMe.show();
-				continue;
 			}
 			else{
 				arrayDropDowns[i].hide();
@@ -83,8 +105,16 @@ $(document).ready(function() {
 	}
 	//this function will create the drop down list with numbers
 	function createDropDown(max, $dropDown){
-	    for (var i=1;i<=max;i++){
-	        $dropDown.append($('<option></option>').val(i).html(i))
+		//for the planets because it starts at 2 for some reason
+		if($dropDown == arrayDropDowns[0]){
+			for (var i=1;i<=max;i++){
+		        $dropDown.append($('<option></option>').val(i+1).html(i))
+		    }
+		}
+		else{
+		    for (var i=1;i<=max;i++){
+		        $dropDown.append($('<option></option>').val(i).html(i))
+		    }
 	    }
 	}
 	//old dropdown iffe
@@ -109,12 +139,15 @@ $(document).ready(function() {
 
 	//ajax for getting each person and displaying its info
 	function generatePeople(numberOfPeople){
-		var temp = number;//saves the last number
-		number = numberOfPeople;//assigns it to the new number required to display
-		//if loop if the new number is smaller than the old one we reset what we have and start over again
-		if(temp < number){
-			infoPeople = "<tr><th>Name</th><th>Gender</th> <th>Height</th><th>Mass</th></tr>";
-		}
+		// var temp = number;//saves the last number
+		// number = numberOfPeople;//assigns it to the new number required to display
+		// //if loop if the new number is smaller than the old one we reset what we have and start over again
+		// if(temp < number){
+		// 	infoPeople = "<tr><th>Name</th><th>Gender</th> <th>Height</th><th>Mass</th></tr>";
+		// }
+		// if(comingFromBigToSmallInDropDownList(numberOfPeople)){
+		// 	infoPeople = "<tr><th>Name</th><th>Gender</th> <th>Height</th><th>Mass</th></tr>";
+		// }
 		//for loop that will run for the number that comes from the drop down list
 		for(var i=1; i <= numberOfPeople; i++){
 			//if that fixes error of the API that does not have a 17 character
@@ -170,6 +203,7 @@ $(document).ready(function() {
 			// $divContent.html("<span id='spanInfo'></span><select class='1-100'></select>");
 			$spanInfo.html("");
 			$dropDownPeople.html("");
+			$dropDownPlanets.html("");
 			linkClicked($type);
 
 		});
